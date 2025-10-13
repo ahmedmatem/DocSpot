9aaef45f716ded68788120ad0243aa8d1a85800f
Ahmed Matem <ahmedmatem@gmail.com>
2025-10-13 22:42:37 +0300

    Add appointment section

--- src/app/core/layouts/main/main.component.ts
+++ src/app/core/layouts/main/main.component.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from "./sections/hero/hero.component";
 import { AboutComponent } from "./sections/about/about.component";
 import { StatsComponent } from "./sections/stats/stats.component";
 import { ServicesComponent } from "./sections/services/services.component";
+import { AppointmentComponent } from "./sections/appointment/appointment.component";
 
 @Component({
   standalone: true,
   selector: 'app-main',
-  imports: [HeroComponent, AboutComponent, StatsComponent, ServicesComponent],
+  imports: [HeroComponent, AboutComponent, StatsComponent, ServicesComponent, AppointmentComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
