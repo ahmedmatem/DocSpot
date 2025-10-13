94cbad07116ca600498fe68269876bb26895c90e
Ahmed Matem <ahmedmatem@gmail.com>
2025-10-13 15:04:27 +0300

    Add services section

--- src/app/core/layouts/main/main.component.ts
+++ src/app/core/layouts/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./sections/hero/hero.component";
 import { AboutComponent } from "./sections/about/about.component";
 import { StatsComponent } from "./sections/stats/stats.component";
+import { ServicesComponent } from "./sections/services/services.component";
 
 @Component({
   standalone: true,
   selector: 'app-main',
-  imports: [HeroComponent, AboutComponent, StatsComponent],
+  imports: [HeroComponent, AboutComponent, StatsComponent, ServicesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
