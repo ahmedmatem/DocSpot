ebbc6298328ffdd28d930dff5b3119e40852df46
Ahmed Matem <ahmedmatem@gmail.com>
2025-10-13 10:37:48 +0300

    Add about section

--- src/app/core/layouts/main/main.component.ts
+++ src/app/core/layouts/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./sections/hero/hero.component";
+import { AboutComponent } from "./sections/about/about.component";
 
 @Component({
   standalone: true,
   selector: 'app-main',
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
