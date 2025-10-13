912f69b139167143275ecedc93ac6d6f5912575e
Ahmed Matem <ahmedmatem@gmail.com>
2025-10-13 11:16:43 +0300

    Add stats section

--- src/app/core/layouts/main/main.component.ts
+++ src/app/core/layouts/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./sections/hero/hero.component";
 import { AboutComponent } from "./sections/about/about.component";
+import { StatsComponent } from "./sections/stats/stats.component";
 
 @Component({
   standalone: true,
   selector: 'app-main',
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, StatsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
---
+++ src/app/core/layouts/main/sections/stats/stats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-stats',
+  imports: [],
+  templateUrl: './stats.component.html',
+  styleUrl: './stats.component.css'
+})
+export class StatsComponent {
+
+}
