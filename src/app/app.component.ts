62ebda73ffdb9fe4c5ec211d5798e6672adbd812
Ahmed Matem <ahmedmatem@gmail.com>
2025-10-18 18:31:14 +0300

    Integrate AOS for animation on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { HeaderComponent } from "./core/layouts/header/header.component";
 import { MainComponent } from "./core/layouts/main/main.component";
 import { ScrollTopComponent } from "./core/layouts/scroll-top/scroll-top.component";
 import { FooterComponent } from "./core/layouts/footer/footer.component";
+import { isPlatformBrowser } from '@angular/common';
+import * as AOS from 'aos';
 
 @Component({
   standalone: true,
@@ -17,4 +19,18 @@ import { FooterComponent } from "./core/layouts/footer/footer.component";
 })
 export class AppComponent {
   title = 'docspot3';
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 600,
+        easing: 'ease-in-out',
+        once: true,      // animate only once
+        // offset: 120,
+        mirror: false    // do not animate while scrolling past back
+      });
+    }
+  }
 }
