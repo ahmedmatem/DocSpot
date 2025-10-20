64de713a3867af393b5d1b5aff040f7db88e3410
Ahmed Matem <ahmedmatem@gmail.com>
2025-10-20 21:19:15 +0300

    Add Material datepicker

--- src/app/core/layouts/main/sections/appointment/appointment.component.ts
+++ src/app/core/layouts/main/sections/appointment/appointment.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   standalone: true,
   selector: 'app-appointment',
-  imports: [],
+  imports: [MatCardModule,MatDatepickerModule],
   templateUrl: './appointment.component.html',
   styleUrl: './appointment.component.css'
 })
 export class AppointmentComponent {
-
+  selected = model<Date | null>(null);
+  today = new Date();
+  minDate = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate()); // start of today
+  maxDate = new Date(this.today.getFullYear() + 1, this.today.getMonth(), this.today.getDate()); // +1 years, same month/day
 }
