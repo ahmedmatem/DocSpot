d4007511dadf03d9491a67780bfaeeecbe39c265
Ahmed Matem <ahmedmatem@gmail.com>
2025-11-14 18:52:37 +0200

    Add AdminWeekSchedule component with routing setup and basic template

---
+++ src/app/features/admin/pages/admin-week-schedule/admin-week-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-week-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-week-schedule.component.html',
+  styleUrl: './admin-week-schedule.component.css'
+})
+export class AdminWeekScheduleComponent {
+
+}
--- src/app/features/admin/pages/admin-week-schedule/week-schedule.routes.ts
+++ src/app/features/admin/pages/admin-week-schedule/week-schedule.routes.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
 import { WeekScheduleComponent } from './tabs/week-schedule/week-schedule.component';
+import { AdminWeekScheduleComponent } from './admin-week-schedule.component';
 
 export const WEEK_SCHEDULE_ROUTES = [
+    {
+        path: '',
+        loadComponent: () => import('./admin-week-schedule.component').then(m => m.AdminWeekScheduleComponent)
+    },
     {
         path: 'create',
         loadComponent: () => import('./tabs/create-week-schedule/create-week-schedule.component')
