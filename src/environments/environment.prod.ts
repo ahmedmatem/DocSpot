f7c0cbfc4040caee5f26e65ebb591713084134f7
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-04 21:36:07 +0200

    Update environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://docspot.up.railway.app'
+  apiBaseUrl: 'https://docspot.up.railway.app',
+  apiAdminBaseUrl: 'https://docspot.up.railway.app/api/admin', // base URL for API calls
+  contactPhone: '+359888123456', // contact phone number
+  contactEmail: 'support@docspot.bg' // contact email address
 };
