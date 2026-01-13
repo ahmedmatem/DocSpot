a43b427397ae96682e830c6dd959553903dadde6
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-13 22:36:06 +0200

    Fix contact info

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://docspot.up.railway.app/api',
   apiAdminBaseUrl: 'https://docspot.up.railway.app/api/admin', // base URL for API calls
-  contactPhone: '+359888123456', // contact phone number
-  contactEmail: 'support@docspot.bg' // contact email address
+  contactPhone: '+359 89 751 0036', // contact phone number
+  contactEmail: 'kadrietopchu@gmail.com' // contact email address
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false, // set to true in production environment file
   apiBaseUrl: 'https://localhost:7168/api', // base URL for API calls
   apiAdminBaseUrl: 'https://localhost:7168/api/admin', // base URL for API calls
-  contactPhone: '+359888123456', // contact phone number
-  contactEmail: 'support@docspot.bg' // contact email address
+  contactPhone: '+359 89 751 0036', // contact phone number
+  contactEmail: 'kadrietopchu@gmail.com' // contact email address
 };
