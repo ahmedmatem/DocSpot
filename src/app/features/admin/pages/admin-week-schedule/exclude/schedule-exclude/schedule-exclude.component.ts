703a87604d2cd6bcd4d06180b94699671122e213
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-17 20:36:16 +0200

    Generate component schedule-exclude

---
+++ src/app/features/admin/pages/admin-week-schedule/exclude/schedule-exclude/schedule-exclude.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-exclude',
+  imports: [],
+  templateUrl: './schedule-exclude.component.html',
+  styleUrl: './schedule-exclude.component.css'
+})
+export class ScheduleExcludeComponent {
+
+}
