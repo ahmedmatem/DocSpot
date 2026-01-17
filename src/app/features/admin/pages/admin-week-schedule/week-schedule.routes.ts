a1a7f7446c6c953176fbe84f400237b6e9051541
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-17 20:41:34 +0200

    Add schedule exclude route

--- src/app/features/admin/pages/admin-week-schedule/week-schedule.routes.ts
+++ src/app/features/admin/pages/admin-week-schedule/week-schedule.routes.ts
@@ -11,6 +11,11 @@ export const WEEK_SCHEDULE_ROUTES = [
             .then(m => m.CreateWeekScheduleComponent)
     },
     {
+        path: 'exclude',
+        loadComponent: () => import('./exclude/schedule-exclude/schedule-exclude.component')
+            .then(m => m.ScheduleExcludeComponent)
+    },
+    {
         path: ':start',
         loadComponent: () => import('./admin-week-schedule.component')
             .then(m => m.AdminWeekScheduleComponent)
